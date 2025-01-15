f935cd4588102418891fc9d0cde189940043e5d0
ArchanaPagul <archanapagul2004@gmail.com>
2025-01-15 17:07:18 +0530

    Update VotingEligibility.java

--- VotingEligibility.java
+++ VotingEligibility.java
@@ -7,15 +7,13 @@ class VotingEligibility {
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in); // Creating a Scanner object for user input
 
-        System.out.println("Enter your age:"); // Prompt the user to enter their age
-        int age = sc.nextInt(); // Reading the user's age and storing it in the variable 'age'
+        System.out.println("Enter your age:"); 
+        int age = sc.nextInt(); 
 
         // Check if the user is eligible for voting
         if (age >18) { 
-            // If age is 18 or above, the candidate is eligible
             System.out.println("You are eligible to vote.");
         } else { 
-            // If age is below 18, the candidate is not eligible
             System.out.println("You are not eligible to vote.");
         }
     }
