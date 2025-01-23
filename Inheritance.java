aa81a620295ef5fdf6d939b97980281531110f5c
ArchanaPagul <archanapagul2004@gmail.com>
2025-01-23 08:50:48 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,41 @@
+// Parent class Vehicle
+class Vehicle {
+    // Attributes of the Vehicle class
+    String brand;
+    String model;
+    int year;
+
+    // Method to represent driving action
+    public void drive() {
+        System.out.println("The " + brand + " " + model + " is now driving.");
+    }
+}
+
+// Child class Car that inherits from Vehicle
+class Car extends Vehicle {
+    // Attribute to the Car class
+    String color;
+
+    public void honk() {
+        System.out.println("The " + color + " " + brand + " " + model + " honks its horn!");
+    }
+}
+
+public class Inheritance{
+    public static void main(String[] args) {
+        // Creating an object of the Car class
+        Car myCar = new Car();
+
+        // Assigning values to the attributes of the Car object
+        myCar.brand = "Toyota";
+        myCar.model = "Corolla";
+        myCar.year = 2022;
+        myCar.color = "Red";
+
+        // Calling the drive method from the Vehicle class
+        myCar.drive();
+
+        // Calling the honk method from the Car class
+        myCar.honk();
+    }
+}
